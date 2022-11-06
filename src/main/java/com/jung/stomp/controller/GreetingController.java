efb945f7f3a53a26082bf35d108c96d09746bffc
yongjung95 <yongjung95@gmail.com>
2022-11-06 18:58:33 +0900

    Initial commit

---
+++ src/main/java/com/jung/stomp/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.jung.stomp.controller;
+
+import com.jung.stomp.Greeting;
+import com.jung.stomp.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
